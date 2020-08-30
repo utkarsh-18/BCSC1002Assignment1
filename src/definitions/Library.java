198c1a2284127af88df8339461f2f16968697acd
Utkarsh Ojha <68592956+utkarsh-18@users.noreply.github.com>
2020-08-30 22:50:17 +0530

    Update Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,46 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Utkarsh Ojha
+ *  Date: 28/08/20
+ *  Time: 5:50 PM
  *  File Name : Library.java
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+    private Book[] currentlyAvailableBooks;
+
+    public Library(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
+
+    public Book[] getCurrentlyAvailableBooks() {
+        return currentlyAvailableBooks;
+    }
+
+    public void setCurrentlyAvailableBooks(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(currentlyAvailableBooks, library.currentlyAvailableBooks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(currentlyAvailableBooks);
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "currentlyAvailableBooks=" + Arrays.toString(currentlyAvailableBooks) +
+                '}';
+    }
 }
+
